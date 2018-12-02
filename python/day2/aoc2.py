4313cbb2811ad3c335da746b33b4fe503988497a
ChrisA <auld@formlabs.com>
2018-12-02 05:19:22 +0000

    working, but dirty, day2 done in Python

---
+++ python/day2/aoc2.py
@@ -0,0 +1,46 @@
+with open("/Users/christopherauld/Desktop/AOC2018/data/day2/input.txt", "r") as file:
+    s = [line.strip("\n") for line in file]
+
+# Part 1
+out = {}
+
+for group in s:
+    found = False
+    for char in group:
+        if group.count(char) > 1 and not found:
+            out[group] = [0, 0]
+            found = True
+        if group.count(char) == 2:
+            out[group][0] = 1
+        if group.count(char) == 3:
+            out[group][1] = 1
+
+twos = 0
+threes = 0
+for key, val in out.items():
+    twos += val[0]
+    threes += val[1]
+
+# Part 2
+o = {}
+for i, group in enumerate(s):
+    o[i] = group
+
+def is_off_by_one(a, b):
+    num_wrong = 0
+    for i, char in enumerate(a):
+        if char != b[i]:
+            num_wrong += 1
+        if num_wrong > 1:
+            return False
+    if num_wrong == 1:
+        return True
+    if num_wrong != 1:
+        return False
+
+
+for key, val in o.items():
+    for k, v in o.items():
+        found = is_off_by_one(val, v)
+        if found:
+            print(k, v, key, val)
