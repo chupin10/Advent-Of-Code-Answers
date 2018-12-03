86c8e11d0c27dd598b5266d244f47e281fe7a9ff
ChrisA <auld@formlabs.com>
2018-12-03 01:23:25 +0000

    add working version for day 2 part 2 in Go

--- go/day2/aoc2.go
+++ go/day2/aoc2.go
@@ -7,6 +7,25 @@ import (
     "strings"
 )
 
+func IsOffByOne(a string, b string) bool {
+    num_wrong := 0
+    for i := range a {
+        a_char := string(a[i])
+        b_char := string(b[i])
+        if a_char != b_char {
+            num_wrong++
+        }
+        if num_wrong > 1 {
+            return false
+        }
+    }
+    if num_wrong == 1 {
+        return true
+    } else {
+        return false
+    }
+}
+
 func main() {
     filename := "/Users/christopherauld/Desktop/AOC2018/data/day2/input.txt"
     b, err := os.Open(filename)
@@ -39,4 +58,13 @@ func main() {
         }
     }
     fmt.Println(twos * threes)
+
+    // Part 2
+    for i = range labels {
+        for j := range labels {
+            if IsOffByOne(labels[i], labels[j]) {
+                fmt.Println(labels[i], labels[j])
+            }
+        }
+    }
 }
