bf7e844d8c9ff3793c026746d032c49187651ace
Chris Auld <auld.chris.10@gmail.com>
2020-12-06 11:40:27 -0500

    day 6 p2

--- AOC2020/day_06/day6.py
+++ AOC2020/day_06/day6.py
@@ -15,28 +15,42 @@ a
 a
 
 b"""
-TEST_ANSWERS = [[3, 3, 3, 1, 1], 11]
+TEST_ANSWERS_1 = [[3, 3, 3, 1, 1], 11]
+TEST_ANSWERS_2 = [[3, 0, 1, 1, 1], 6]
 
 
 def open_input() -> list[str]:
+    # NOTE: you need to have 2 blank lines at the end of input.txt
     with open(f'{GIT_DIR}/day_06/input.txt', 'r') as fh:
         lines = fh.read()
-        groups = [line.replace('\n', '') for line in lines.split('\n\n')]
+        groups = [(line.count('\n') + 1, line.replace('\n', ''))
+                  for line in lines.split('\n\n')]
         return groups
 
 
-def numyes(group: str) -> int:
-    return len(set(group))
+def numyes(_group: str) -> int:
+    return len(set(_group))
+
+
+def num_unanimous(_group: str, grpsz: int) -> int:
+    if grpsz == 1:
+        return len(set(_group))
+    un = 0
+    for char in set(_group):
+        if _group.count(char) == grpsz:
+            un += 1
+    return un
 
 
 if __name__ == '__main__':
-    groups = [line.replace('\n', '') for line in TEST.split('\n\n')]
+    groups = [(line.count('\n') + 1, line.replace('\n', ''))
+              for line in TEST.split('\n\n')]
     yeses = []
-    for group, answer in zip(groups, TEST_ANSWERS[0]):
-        n = numyes(group)
-        assert n == answer
+    for (groupsize, group), answer in zip(groups, TEST_ANSWERS_2[0]):
+        n = num_unanimous(group, groupsize)
+        assert n == answer, f'{n=}\t{answer=}'
         yeses.append(n)
-    assert sum(yeses) == TEST_ANSWERS[1]
+    assert sum(yeses) == TEST_ANSWERS_2[1]
 
     groups = open_input()
-    print(sum([numyes(group) for group in groups]))
+    print(sum([num_unanimous(group, groupsize) for (groupsize, group) in groups]))
